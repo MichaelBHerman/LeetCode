c5c3e6fda70eb3c8c55a24c6e5722da27421c1f7
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-07-14 14:38:16 -0600

    Time: 85 ms (5.23%), Space: 44.9 MB (5.03%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  let h = haystack.length;
+  let n = needle.length;
+  let maxIdxToCheck = h - n;
+
+    for (let i = 0; i <= maxIdxToCheck; i++) {
+      let subString = haystack.substring(i, i + n)
+      console.log(subString)
+      if (subString === needle) {
+        return i;
+      }
+    }
+    return -1;
+};
