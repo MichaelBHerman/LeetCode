b6257a5d9260fe22002916d9a7151b2cde19ac5a
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-07-19 15:17:03 -0600

    Time: 50 ms (95.77%), Space: 42.3 MB (50.81%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,11 +4,9 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let jewelsArr = [];
+    let jewelsArr = Array.from( jewels );
     let jewelCount = 0;
-    for (let i = 0; i <= jewels.length - 1; i++) {
-      jewelsArr[i] = jewels[i];
-    }
+    
     for (let j = 0; j <= stones.length - 1; j++) {
       if (jewelsArr.includes(stones[j])) {
         jewelCount++;
