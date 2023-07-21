29cfeb10aac0d4288e2a81281909331d3df93cf7
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-07-21 16:32:13 -0600

    Time: 1 ms (100.00%), Space: 43.4 MB (38.15%) - LeetHub

--- 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.java
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.java
@@ -1,12 +1,12 @@
 class Solution {
     public int sumOfSquares(int[] nums) {
-       int n = nums.length;
-       int sumOfSquares = 0;
-       for (int i = 0; i < nums.length; i++) {
-           if (n % (i + 1) == 0) {
-               sumOfSquares += nums[i] * nums[i];
-           }
-       }
-       return sumOfSquares;
-   }
+        int n = nums.length;
+        int sumOfSquares = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (n % (i + 1) == 0) {
+                sumOfSquares += nums[i] * nums[i];
+            }
+        }
+        return sumOfSquares;
+    }
 }
