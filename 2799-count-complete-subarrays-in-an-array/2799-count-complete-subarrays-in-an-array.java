b02617b92e051da7763d8d1b717cce0ad1eb219f
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-07-31 00:37:58 -0600

    Time: 204 ms (100.00%), Space: 43.5 MB (100.00%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -2,17 +2,17 @@ class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int completeSubArrayCount = 0;
         HashSet<Integer> set =new HashSet<>();
-        for(int n :nums) {
+        for (int n :nums) {
             set.add(n);
         }
         int distinctElements = set.size();
-        
-        for(int i = 0; i < nums.length; i++) {
+
+        for (int i = 0; i < nums.length; i++) {
             HashSet<Integer> set2 =new HashSet<>();
-            for(int j = i; j < nums.length; j++){
+            for (int j = i; j < nums.length; j++) {
                 set2.add(nums[j]);
-                if(set2.size()==distinctElements){
-// As the condition hits, left all subarrays will also satisfy the condition, so add (nums.length-j) and then break.  
+                if (set2.size() == distinctElements) {
+// As the condition hits, all subarrays to the right will also satisfy the condition, so add (nums.length-j) and then break.
                     completeSubArrayCount += nums.length - j;
                     break;
                 }
