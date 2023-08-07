3dda669310fdb907ccae47a58667f86716ae4e9d
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-08-07 11:34:17 -0600

    Time: 4 ms (100.00%), Space: 43.7 MB (100.00%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,21 @@
+class Solution {
+  public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 'i') {
+                sb.append(arr[i]);
+            }
+            else {
+                if (sb.length() == 0) {
+                    continue;
+                }
+                String temp = String.valueOf(sb.reverse());
+                sb.setLength(0);
+                sb.append(temp);
+
+            }
+        }
+        return sb.toString();
+    }
+}
