cf5109e50a37932b3a7b9b39c4620f2966ee908f
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-08-22 11:32:07 -0600

    Time: 4 ms (21.53%), Space: 40.6 MB (99.48%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -4,11 +4,11 @@ class Solution {
         int pairs = 0;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                if (nums.get(i) + nums.get(j) < target) {
-                    pairs++;
-                }
                 if (nums.get(i) + nums.get(j) >= target) {
                     break;
+                }
+                else {
+                    pairs++;
                 }
             }
         }
