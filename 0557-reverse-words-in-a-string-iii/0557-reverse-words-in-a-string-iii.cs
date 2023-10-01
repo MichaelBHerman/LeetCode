b1dd754262b4b4f35121dd1c4dcfdd81f971533a
Michael Herman <89651714+MichaelBHerman@users.noreply.github.com>
2023-10-01 13:38:41 -0600

    Time: 111 ms (13.89%), Space: 122.8 MB (6.94%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public string ReverseWords(string s)
+        {
+           
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            string reversed = new string(arr);
+            string[] splitted = reversed.Split(" ");
+            string properOrder = "";
+
+            for (int i = splitted.Length - 1; i >= 0; i--)
+                
+            {
+                if (i != splitted.Length - 1)
+                {
+                    properOrder += " ";
+                }
+                properOrder += splitted[i];
+            }
+            Console.WriteLine(properOrder);
+            return properOrder;
+        }
+}
